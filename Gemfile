d424e296d7e58dbc2cfc8561eff5c033e526b20b
Kohei0214 <kkishimotok@gmail.com>
2018-07-19 11:37:30 +0900

    gem | install and setting meta_tags

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'haml-rails'
 gem 'erb2haml'
 
 gem 'font-awesome-rails'
+gem 'meta-tags'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,44 @@
 module ApplicationHelper
+  #titleを動的に生成
+  # def title
+  #   title = @matome.title if @matome
+  # end
+  #
+  # #descriptionを動的に生成
+  # def description
+  #   description = @matome.description if @matome
+  # end
+
+  def default_meta_tags
+    {
+      site: 'なろうまとめ',
+      # title: title,
+      title: 'なろうのおすすめ',
+      reverse: true,
+      charset: 'utf-8',
+      # description: description,
+      description: 'みんなで作る小説家になろうオススメランキングサイト',
+      keywords: '小説家になろう, なろう, なろう小説, おすすめ, オススメ, まとめ, なま, キュレーション, 共有',
+      canonical: request.original_url,
+      separator: ':',
+      icon: [
+        # { href: image_url('favicon.ico') },
+        # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ]
+      # ,
+      # og: {
+      #   site_name: 'なろうまとめ',
+      #   title: title,
+      #   description: description,
+      #   type: 'website',
+      #   url: request.original_url,
+      #   # image: image_url('ogp.png'),
+      #   locale: 'ja_JP',
+      # },
+      # twitter: {
+      #   card: 'summary',
+      #   site: '@ツイッターのアカウント名',
+      # }
+    }
+  end
 end
