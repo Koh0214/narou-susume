4c1fcd4da05f9cb953236c9882d7bfb69d0e27e0
Kohei0214 <kkishimotok@gmail.com>
2018-07-24 14:58:51 +0900

    routes.rb | fix root_path

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :novels
   resources :osusumes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "osusumes#show"
+  root "osusumes#index"
 end
