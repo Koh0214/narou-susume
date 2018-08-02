2b4904ef58674ed40dc7a6cdcd7ab5c7e2cc686a
Kohei0214 <kkishimotok@gmail.com>
2018-08-02 23:19:41 +0900

    osusumes_controller | add @osusumes = Osusume.all

--- app/controllers/osusumes_controller.rb
+++ app/controllers/osusumes_controller.rb
@@ -10,6 +10,7 @@ class OsusumesController < ApplicationController
   # GET /osusumes/1
   # GET /osusumes/1.json
   def show
+    @osusumes = Osusume.all.shuffle.take(8)
     @osusume = Osusume.where(:id => params[:id]).first
 
     # herokuでlikeがnilのレコードが先にきちゃうので、以下のようにして修正
