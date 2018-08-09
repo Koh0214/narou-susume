075b92860b990edf8b0e8203154d3aa79a63342e
Kohei0214 <kkishimotok@gmail.com>
2018-08-09 20:22:59 +0900

    Review migration | add default=0 to like column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_093513) do
+ActiveRecord::Schema.define(version: 2018_08_09_112043) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2018_08_09_093513) do
   create_table "reviews", force: :cascade do |t|
     t.string "name"
     t.text "text"
-    t.integer "like"
+    t.integer "like", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "novel_id"
