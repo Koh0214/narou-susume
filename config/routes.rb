c0af26cc81a4c3614d001bb17fa3ea3a7c90be2c
Kohei0214 <kkishimotok@gmail.com>
2018-08-09 20:04:45 +0900

    routes.rb | nest revies into novels

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :reviews
   get "osusumes/get_novel_info"
   get "osusumes/like"
   get "novels/comment_count_up"
   get "novels/comment_count_down"
+
   resources :comments
-  resources :novels
+  resources :novels do
+    resources :reviews
+  end
   resources :osusumes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "osusumes#index"
